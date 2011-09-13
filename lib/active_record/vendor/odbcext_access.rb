d372dc1dbea7e4c5b109e340c8ab1cee85b00694
jmozmoz <joachim.herb@gmx.de>
2011-09-14 00:26:51 +0200

    Another hack for last_insert_id for MS Access vendor

--- lib/active_record/vendor/odbcext_access.rb
+++ lib/active_record/vendor/odbcext_access.rb
@@ -24,29 +24,30 @@
 require 'active_record/connection_adapters/abstract_adapter'
 
 module ODBCExt
-  
+
   # ------------------------------------------------------------------------
   # Mandatory methods
   #
-  
+
   # #last_insert_id must be implemented for any database which returns
   # false from #prefetch_primary_key?
   def last_insert_id(table, sequence_name, stmt = nil)
     @logger.unknown("ODBCAdapter#last_insert_id>") if @trace
-    select_value("select @@IDENTITY", 'last_insert_id')
+    select_value("select max(#{quote_column_name(sequence_name)}) " +
+                 "from #{quote_table_name(table)}", 'last_insert_id')
   end
 
   # ------------------------------------------------------------------------
   # Method redefinitions
   #
-  # DBMS specific methods which override the default implementation 
+  # DBMS specific methods which override the default implementation
   # provided by the ODBCAdapter core.
 
   def add_column_options!(name, options = {})
     @logger.unknown("ODBCAdapter#add_column_options overloaded>") if @trace
     @logger.unknown("name=#{name}") if @trace
     if options.has_key?(:default) then
-      options.delete(:default) 
+      options.delete(:default)
       # FIXME: Call ALTER TABLE via JetDB or something else to set default!
       # see e.g. http://goo.gl/9wqH9
     end
@@ -60,7 +61,7 @@ module ODBCExt
     @logger.unknown("ODBCAdapter#type_to_sql overloaded>") if @trace
     @logger.unknown("args=[#{type}|#{limit}|#{precision}|#{scale}]") if @trace
     super(type, limit)
-    # FIXME: Call ALTER TABLE via ADO or something else to set 
+    # FIXME: Call ALTER TABLE via ADO or something else to set
     # precision and scale! See e.g. http://goo.gl/CPI45
   rescue Exception => e
     @logger.unknown("exception=#{e}") if @trace
